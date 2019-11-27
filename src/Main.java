d80b093a58f18244fc129f613e5272b6e5f5ed04
Вячеслав Алексеевич <>
2019-11-27 17:43:12 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,8 @@ import java.io.InputStreamReader;
 
 public class Main {
     enum RomanNumbers {
-        I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50),
-        XC(90), C(100), CD(400), D(500), CM(900), M(1000);
+        I(1), II(2), III(3), IV(4), V(5),
+        VI(6), VII(7), VIII(8), IX(9), X(10);
 
         private int value;
 
